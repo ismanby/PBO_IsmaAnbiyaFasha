9e88281a074ddd8dc932bf8989d5e38875196a35
ismanby <ismaanby@gmail.com>
2026-09-07 14:59:19 +0700

    [Jobsheet 3] Percobaan 2 - Modifikasi Pertanyaan 5

--- Jobsheet 3/Motor.java
+++ Jobsheet 3/Motor.java
@@ -28,6 +28,8 @@ public class Motor {
             System.out.println("Kecepatan tidak boleh lebih dari 0 jika mesin off.");
         } else if (kecepatan > 100) {
             System.out.println("Kecepatan maksimal adalah 100");
+        } else if (kecepatan < 0) { 
+            System.out.println("Kecepatan tidak boleh bernilai negatif");
         } else {
             this.kecepatan = kecepatan;
         }
